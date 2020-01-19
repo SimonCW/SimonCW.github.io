77b0e4f7e8ca1b3b2699901038bfefe03cc9b62b
Simon Weiss <simon.weiss@alexanderthamm.com>
2020-01-19 11:22:50 +0100

    Add image and social

--- src/blog/pelicanconf.py
+++ src/blog/pelicanconf.py
@@ -2,15 +2,17 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Simon Weiss'
+AUTHOR = "Simon Weiss"
 SITENAME = "Simon's Landingzone"
-SITEURL = ''
+SITEURL = "https://simoncw.github.io"
+SITELOGO = "images/profile/simoncw_profile.jpg"
+PATH = "content"
 
-PATH = 'content'
+STATIC_PATHS = ["images"]
 
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = "Europe/Paris"
 
-DEFAULT_LANG = 'en'
+DEFAULT_LANG = "en"
 
 THEME = "/home/simon/repos/other/themes/Flex"
 
@@ -22,16 +24,20 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+#LINKS = (("Pelican", "http://getpelican.com/"),
+#         ("Python.org", "http://python.org/"),
+#         ("Jinja2", "http://jinja.pocoo.org/"))
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+
+SOCIAL = (
+    ("linkedin", "www.linkedin.com/in/weiss-simon"),
+    ("twitter", "https://twitter.com/SimonCWeiss"),
+    ("github", "https://github.com/SimonCW"),
+    ("stack-overflow", "https://stackoverflow.com/users/6663432/simoncw"),
+)
